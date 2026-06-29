4ab240ed0b3e2b516827b32ac45d727c5aa8a971
SayanDolai <dolaisayan03@gmail.com>
2026-06-29 17:29:00 +0530

    Add Dockerfile and .dockerignore; update server.js for static file serving

--- Backend/server.js
+++ Backend/server.js
@@ -3,16 +3,26 @@ import 'dotenv/config'
 import {clerkMiddleware} from '@clerk/express'
 import connectDB from './src/lib/db.js'
 import cors from 'cors'
+import fs from "fs"
+import path from "path"
 
 const app = express() 
 const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL
+const publicDir = path.join(process.cwd ,"public")
 app.use(express.json()) //data comes from client
 app.use(clerkMiddleware()) //check the authentication
 app.use(cors({origin:FRONTEND_URL , credentials:true})) // 
 app.get("/",(req,res)=>{
     res.status(200).json("ok:true")
 })
+
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir))
+    app.get('/{*any}',(req,res,next)=>{
+        res.sendFile(path.join(publicDir,"index.html"),(err)=>next(err))
+    })
+}
 app.listen(PORT,()=>{
     connectDB()
     console.log(`server is running at : https//localhost:${PORT}`)
